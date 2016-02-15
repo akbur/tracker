357e96a283cb6d24093592326e597cf2aefa2a7d
Kris Burke <kris.burke611@gmail.com>
2016-02-15 16:48:15 -0500

    Starter angular frontend with no content yet

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
 var port = process.env.PORT || 1337;
 
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.render('/index');
 })
 
 app.listen(port);
