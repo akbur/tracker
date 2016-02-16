78a084409a79925a74cef3b6f9031c9d57b4afd1
Kris Burke <kris.burke611@gmail.com>
2016-02-16 01:31:30 -0500

    Add body parser express middleware.

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ var morgan = require('morgan');
 var app = express();
 var port = process.env.PORT || 1337;
 var db = require('./app/config');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-//configuration
+//config
+
+//set static file location
 app.use(express.static(__dirname + '/public'));
+//log requests to the console
 app.use(morgan('dev'));
+//parse app
+app.use(bodyParser.urlencoded({'extended':'true'}));        
+app.use(bodyParser.json());                             
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(methodOverride());
 
 //routes
 require('./app/routes.js')(app);
